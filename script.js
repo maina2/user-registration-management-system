88567174d99185e7816b46dc66a2db1b69eccb8e
Simon Chanzu <chanzusimon6@gmail.com>
2023-05-13 23:35:18 +0300

    upload of thr useer registration management system

--- script.js
+++ script.js
@@ -1,59 +1,40 @@
-// var selectedRow = null;
-
-// function onformsub(){
-//     Event.preventDefault();
-//     var formdata = readformdata();
-//     if(selectedRow === null){
-//         InsertRecord(formdata);
-//     }
-//     else{
-
-//     }
 
 
-// }
+function onFormSubmit() {
 
-// function readformdata(){
+    const nameInput = document.getElementById("name");
+    const idNumberInput = document.getElementById("id");
+    const countryInput = document.getElementById("country");
+    const languageInput = document.getElementById("languages");
+  
+    const tableBody = document.getElementById("body");
+    const newRow = tableBody.insertRow(-1);
+    const nameCell = newRow.insertCell(0);
+    const idNumberCell = newRow.insertCell(1);
+    const countryCell = newRow.insertCell(2);
+    const languageCell = newRow.insertCell(3);
+    const actionsCell = newRow.insertCell(4);
+  
+    nameCell.textContent = nameInput.value;
+    idNumberCell.textContent = idNumberInput.value;
+    countryCell.textContent = countryInput.value;
+    languageCell.textContent = languageInput.value;
+    actionsCell.innerHTML =
+      '<button id="green", onclick="editUser(this)">Edit</button> <button onclick="deleteUser(this)">Delete</button>';
 
-//     var formdata = ();
-//     formdata['name'] = document.getElementById('name');
-//     formdata['id'] = document.getElementById('id');
-//     formdata['country'] = document.getElementById('country');
-//     formdata['languages'] = document.getElementById('languages');
-//     return formdata;
-// }
-// function InsertRecord(data){
-//     var table  = document.getElementById('table').getElementsByTagName('tbody')[0];
-//     var newRow = table.insertRow(table.length);
+    nameInput.value = "";
+    idNumberInput.value = "";
+    countryInput.selectedIndex = 0;
+    languageInput.selectedIndex = 0;
 
-//     var cell1 = newRow.insertCell(0);
-//     cell1.innerHTML = data.name;
+  }
 
-//     var cell2 = newRow.insertCell(1);
-//     cell2.innerHTML = data.id;
+  function onEdit(td){
+      selectedRow = td.parentElement.parentElement;
+      document.getElementById('name').value = selectedRow.cells[0].innerHTML;
+      document.getElementById('id').value = selectedRow.cells[1].innerHTML;
+      document.getElementById('country').value = selectedRow.cells[2].innerHTML;
+      document.getElementById('languages').value = selectedRow.cells[3].innerHTML;
 
-//     var cell3 = newRow.insertCell(2);
-//     cell3.innerHTML = data.country;
-
-//     var cell4 = newRow.insertCell(3);
-//     cell4.innerHTML = data.languages;
-
-
-// }
-
-document.addEventListener('documentloaded',function(){
-
-    const table = document.getElementById('table');
-    const forms = document.forms;
-})
-
-table.addEventListener('click', (e) => {
-    if (e.className == 'delete'){
-        const tr = e.target.parentElement;
-        tr.parentNode.removechild(tr)
-    }
-})
-
-
-
+  }
 
